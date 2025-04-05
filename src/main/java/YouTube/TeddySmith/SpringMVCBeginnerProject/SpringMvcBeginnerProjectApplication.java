325f97b5a2943a8f26db1d2e80923d238b355fd7
yamchytskyi <wk.vitalii@gmail.com>
2025-04-05 16:23:23 +0200

    Initial commit -Spring MVC Beginner Project setup, -Postgress db

---
+++ src/main/java/YouTube/TeddySmith/SpringMVCBeginnerProject/SpringMvcBeginnerProjectApplication.java
@@ -0,0 +1,13 @@
+package YouTube.TeddySmith.SpringMVCBeginnerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBeginnerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcBeginnerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/YouTube/TeddySmith/SpringMVCBeginnerProject/SpringMvcBeginnerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package YouTube.TeddySmith.SpringMVCBeginnerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBeginnerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
